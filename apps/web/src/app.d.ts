2a362d2008a71c8ffd47f7a674918e4f18d68540
Evan / Fero <hi@evan.graphics>
2023-08-07 14:02:00 -0700

    feat(web): sveltekit demo with tailwind

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -1,16 +1,12 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-  namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
-    // interface Platform {}
-
-    interface Session {
-      token: string | null;
-    }
-  }
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
 }
 
 export {};
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,22 +1,54 @@
 <script>
-  import 'tailwind-preset-base/global.css';
-  import '$lib/nhost'; // initialize nhost
-
-  import Footer from './Footer.svelte';
-  import GoogleAnalytics from './GoogleAnalytics.svelte';
-  import HeaderNavigation from './HeaderNavigation.svelte';
+	import '../app.postcss';
+	import Header from './Header.svelte';
+	import './styles.css';
 </script>
 
-<GoogleAnalytics />
+<div class="app">
+	<Header />
 
-<div class="flex h-full flex-col">
-  <HeaderNavigation />
+	<main>
+		<slot />
+	</main>
 
-  <div class="flex flex-1 flex-col">
-    <main class="flex-1 px-4 pb-16 pt-[68px] md:pt-10">
-      <slot />
-    </main>
-
-    <Footer />
-  </div>
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
 </div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
