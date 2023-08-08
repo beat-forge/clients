92cd5240801014c95cb415ca6ef54068faf46539
Evan / Fero <hi@evan.graphics>
2023-08-07 20:36:22 -0700

    feat(ui): button pill variant

--- packages/ui/components/button/Button.svelte
+++ packages/ui/components/button/Button.svelte
@@ -7,14 +7,15 @@
   export let customClasses;
 
   const variantClasses = {
-    primary: 'bg-primary-50 hover:bg-primary-100 text-black-950',
-    secondary: 'bg-primary-800 hover:bg-primary-900 text-white',
+    primary: 'px-4 py-2 bg-primary-50 hover:bg-primary-100 text-black-950',
+    secondary: 'px-4 py-2 bg-primary-800 hover:bg-primary-900 text-white',
     download:
-      'hover:-translate-y-[2px] bg-gradient-to-r from-[#47334f] to-[#2d2c4f] text-white duration-[200ms] ease-[cubic-bezier(0.22,0.61,0.36,1)]',
+      'px-4 py-2 hover:-translate-y-[2px] bg-gradient-to-r from-[#47334f] to-[#2d2c4f] text-white duration-[200ms] ease-[cubic-bezier(0.22,0.61,0.36,1)]',
     ghost: 'bg-transparent fill-white text-white',
+    pill: 'text-xs bg-primary-700 w-fit px-2 p-1 text-fore-base whitespace-nowrap',
   };
 
-  let buttonClasses = `disabled:opacity-50 flex flex-row items-center justify-center transition-all duration-[80ms] px-4 py-2 rounded-full gap-2 font-bold text-sm ${variantClasses[variant]} ${customClasses}`;
+  let buttonClasses = `disabled:opacity-50 flex flex-row items-center justify-center transition-all duration-[80ms] rounded-full gap-2 font-bold text-sm ${variantClasses[variant]} ${customClasses}`;
 </script>
 
 <button {disabled} class={buttonClasses}>
--- packages/ui/components/pill/Pill.svelte
+++ packages/ui/components/pill/Pill.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class="rounded-3xl bg-primary-700 w-fit px-2 p-1 text-fore-base flex items-center gap-2 whitespace-nowrap"
+  class="rounded-full bg-primary-700 w-fit px-2 p-1 text-fore-base flex items-center gap-2 whitespace-nowrap"
 >
   {#if $$slots.icon}
     <slot name="icon" />
