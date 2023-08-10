7b5bf2a70fbe5fba20a93e931e08bb49e41bfce6
Evan / Fero <hi@evan.graphics>
2023-08-09 22:04:15 -0700

    feat(ui): initial link component functionality

--- packages/ui/link/Link.svelte
+++ packages/ui/link/Link.svelte
@@ -3,18 +3,20 @@
   // Variants: underlined, text, primarybutton, secondarybutton
   export let variant;
   export let customClasses;
+  // Boolean
+  export let external;
   export let href;
 
   const variantClasses = {
     underlined: 'underline',
     text: 'hover:underline',
-    primarybutton: 'flex flex-row items-center justify-center transition-all duration-[80ms] rounded-full gap-2 font-bold text-sm px-4 py-2 bg-primary-50 hover:bg-primary-100 text-black-950',
-    secondarybutton: 'flex flex-row items-center justify-center transition-all duration-[80ms] rounded-full gap-2 font-bold text-sm px-4 py-2 bg-primary-800 hover:bg-primary-900 text-white'
+    primary: 'flex flex-row items-center justify-center transition-all duration-[80ms] rounded-full gap-2 font-bold text-sm px-4 py-2 bg-primary-50 hover:bg-primary-100 text-primary-950',
+    secondary: 'flex flex-row items-center justify-center transition-all duration-[80ms] rounded-full gap-2 font-bold text-sm px-4 py-2 bg-primary-800 hover:bg-primary-700 text-white'
   };
 
   let linkClasses = `${variantClasses[variant]} ${customClasses}`
 </script>
 
-<a href={href ?? ''}} class={linkClasses}>
+<a href={href ?? ''} class={linkClasses} target={`${external ? '_blank' : ''}`} rel={`${external ? 'noopener noreferrer' : ''}`}>
   <slot />
 </a>
