713c4d661f4ea5760acaba495a04daba2b744ac4
Evan / Fero <hi@evan.graphics>
2023-08-09 22:14:06 -0700

    fix(web): disclaimer clipping with landingvideo

--- apps/web/src/lib/components/layout/NavBarDisclaimer.svelte
+++ apps/web/src/lib/components/layout/NavBarDisclaimer.svelte
@@ -1,4 +1,4 @@
-<div class="beatforge-page-disclaimer flex w-full items-center py-4 px-4 mx-auto z-[100] bg-[#EC892D]">
+<div class="relative beatforge-page-disclaimer flex w-full items-center py-4 px-4 mx-auto z-[100] bg-[#EC892D]">
     <div class="flex items-center justify-center w-full">
         <div class="text-sm font-bold">BeatForge is in development. What you see here does not represent the final product.</div>
     </div>
