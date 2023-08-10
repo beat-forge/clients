8c5d2445265415fd2e60b8bdc122bce481479f10
Evan / Fero <hi@evan.graphics>
2023-08-09 22:10:45 -0700

    feat(web): implement ui/link in navbar

--- apps/web/src/lib/components/layout/NavBar.svelte
+++ apps/web/src/lib/components/layout/NavBar.svelte
@@ -1,11 +1,15 @@
-<div class="beatforge-page-navbar flex w-full items-center py-8 px-4 mx-auto max-w-7xl z-[100]">
+<script>
+	import { Link } from 'ui/link';
+</script>
+
+<div class="beatforge-page-navbar flex w-full items-center py-8 px-4 mx-auto max-w-7xl z-[100] text-sm font-bold">
 	<a href="/" class="flex items-center">
 		<img src="/images/logo.svg" draggable="false" alt="logo" class="h-12 p-3" />
 	</a>
 
 	<div class="ml-auto mr-4 flex flex-row gap-8 items-center">
-		<a href="https://discord.gg/HTVrjFENaZ" class="text-sm font-bold" target="_blank" rel="noopener"
-			>Discord</a
-		>
+		<Link variant="text" href="https://discord.gg/HTVrjFENaZ" external>
+			Discord
+		</Link>
 	</div>
 </div>
