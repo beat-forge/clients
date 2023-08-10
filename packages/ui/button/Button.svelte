eab47077fec8c9228775a6c5dba053a0690526bc
Evan / Fero <hi@evan.graphics>
2023-08-09 21:50:41 -0700

    refactor(ui): refactor package exports

--- packages/ui/button/Button.svelte
+++ packages/ui/button/Button.svelte
@@ -10,7 +10,7 @@
   const variantClasses = {
     primary: 'px-4 py-2 bg-primary-50 hover:bg-primary-100 text-black-950',
     secondary: 'px-4 py-2 bg-primary-800 hover:bg-primary-900 text-white',
-    download:
+    gradient:
       'px-4 py-2 hover:-translate-y-[2px] bg-gradient-to-r from-[#47334f] to-[#2d2c4f] text-white duration-[200ms] ease-[cubic-bezier(0.22,0.61,0.36,1)]',
     ghost: 'bg-transparent fill-white text-white',
     pill: 'text-xs bg-primary-700 w-fit px-2 p-1 text-fore-base whitespace-nowrap',
