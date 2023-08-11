a03a0c28e1188216f37967498869a19b6a259236
Checksum <me@checksum.space>
2023-08-11 18:06:31 -0500

    refactor: remove useless `commands`

--- commands/format-project-words.sh
+++
@@ -1,4 +0,0 @@
-# In repository root
-cd "$(dirname $0)/.."
-
-cat project-words.txt | sed -e '/^$/d' | sort --ignore-case | tr '[A-Z]' '[a-z]' | uniq > project-words.txt.tmp && mv project-words.txt.tmp project-words.txt
