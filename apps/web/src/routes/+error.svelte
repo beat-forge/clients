5c0794344b5373710b6df13c2959004f1209aaaf
David <davidisadev@gmail.com>
2023-08-13 16:15:32 -0500

    fix: fixed env example and fixed error page to be properly centered

--- apps/web/src/routes/+error.svelte
+++ apps/web/src/routes/+error.svelte
@@ -1,3 +1,3 @@
-<div class="flex justify-center align-middle h-screen">
+<div class="w-full h-full flex flex-col items-center px-4">
     <h1 class="text-3xl text-white mt-20">There was an error, lmao</h1>
 </div>
