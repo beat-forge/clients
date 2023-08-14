e05d1144243333bbde6d6aa95ba85b1ea448f6b7
Evan / Fero <hi@evan.graphics>
2023-08-13 22:37:37 -0700

    fix(ui): dropdown menu

--- packages/ui/dropdown/Dropdown.svelte
+++ packages/ui/dropdown/Dropdown.svelte
@@ -7,20 +7,23 @@
       states: { open }
     } = createDropdownMenu()
   </script>
-   
+
 <button use:melt={$trigger}>
     <slot name="trigger"></slot>
 </button>
 {#if $open}
-    <div class="flex flex-col p-2 text-center rounded-lg select-none bg-primary-850 text-primary-50 z-[9999] font-bold" use:melt={$menu} transition:fly={{ duration: 150, y: -10 }}>
-        <div use:melt={$item} class="p-2 rounded-md hover:bg-primary-800">
-            <Link variant="unset" href="/discover">Discover Mods</Link>
+    <div class="flex flex-col p-4 text-center rounded-lg select-none bg-primary-850 text-primary-50 z-[9999] font-bold" use:melt={$menu} transition:fly={{ duration: 150, y: -10 }}>
+        <div use:melt={$item} class="rounded-md hover:bg-primary-800">
+            <Link customClasses="flex h-full w-full p-2" variant="unset" href="/">Home</Link>
         </div>
-        <div use:melt={$item} class="p-2 rounded-md hover:bg-primary-800">
-            <Link variant="unset" href="/download">Client</Link>
+        <div use:melt={$item} class="rounded-md hover:bg-primary-800">
+            <Link customClasses="flex h-full w-full p-2" variant="unset" href="/discover">Discover Mods</Link>
         </div>
-        <div use:melt={$item} class="p-2 rounded-md hover:bg-primary-800">
-            <Link variant="unset" href="https://discord.com/invite/HTVrjFENaZ">Discord</Link>
+        <div use:melt={$item} class="rounded-md hover:bg-primary-800">
+            <Link customClasses="flex h-full w-full p-2" variant="unset" href="/client">Client</Link>
+        </div>
+        <div use:melt={$item} class="rounded-md hover:bg-primary-800">
+            <Link customClasses="flex h-full w-full p-2" variant="unset" href="https://discord.com/invite/HTVrjFENaZ">Discord</Link>
         </div>
     </div>
 {/if}
