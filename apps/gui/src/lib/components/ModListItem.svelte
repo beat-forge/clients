4962b508a3ca3943f7cdc201828bba9fb4f4cd44
David <davidisadev@gmail.com>
2023-08-18 16:30:46 -0500

    fix: switched to infinite scrolling and game version filter
    Co-authored-by: fero <hi@evan.graphics>
    Co-authored-by: ChecksumDev <me@checksum.space>

--- apps/gui/src/lib/components/ModListItem.svelte
+++ apps/gui/src/lib/components/ModListItem.svelte
@@ -1,69 +1,57 @@
 <script lang="ts">
-	import Sugar from 'sugar';
-	import { CalendarIcon, DownloadIcon, LogoIcon, NotVerifiedIcon, VerifiedIcon, CogIcon, CubeIcon, ChevronDownIcon, CategoryIcon } from 'ui/icons';
-	import { Button } from 'ui/button';
-	import { Pill } from 'ui/pill';
-    import { Link } from 'ui/link';
-
-    import { slide, fly } from 'svelte/transition';
-    import { quintOut } from 'svelte/easing';
-
+	import { DownloadIcon } from 'ui/icons';
+        
 	export let name: string;
-    export let slug: string;
+	export let slug: string;
 	export let author: {
 		username: string;
 	};
 	export let description: string;
 	export let category: string;
 	export let downloads: any;
-	export let updatedAt: number
 	export let icon: string;
-
-    let updatedAtProper: string;
-    try {
-        updatedAtProper = Sugar.Date.relative(new Date(updatedAt * 1000))
-    } catch (e) {
-        // @ts-ignore
-        updatedAtProper = Sugar.Date.relative(new Date(Date.parse(updatedAt * 1000)))
-    }
 </script>
 
 <a href={`/mod/${slug}`}>
-    <div
-	    class="beatforge-discover-list-item flex flex-row gap-2 md:gap-4 w-full bg-primary-800 hover:bg-primary-850 border-b-[1px] border-primary-850 items-center p-2 md:p-4 min-w-0 transition hover:duration-[0ms] duration-[120ms] my-2 md:my-0 rounded-lg md:rounded-none">
-        <div
-            class="beatforge-discover-list-item-image h-12 w-12 md:h-16 md:w-16 bg-primary-600 flex flex-shrink-0 overflow-hidden rounded-md mr-2"
-        >
-            <img
-                alt={`Listing for mod ${name}, created by ${author}`}
-                src={icon ? icon : '/images/unknown.svg'}
-                class="w-full h-full object-fill"
-            />
-        </div>
-        <div class="beatforge-discover-list-item-info w-full flex flex-col gap-1 md:gap-1 min-w-0 truncate">
-            <div class="beatforge-discover-list-item-info-title flex items-center gap-2">
-                <h2 class="font-black text-md md:text-xl">{name}</h2>
-                <p class="text-xs text-primary-200">by {author}</p>
-            </div>
-            <div class="beatforge-discover-list-item-description flex min-w-0">
-                <p class="text-xs text-primary-200 truncate min-w-0 font-medium">
-                    {description}
-                </p>
-            </div>
-        </div>
-        <div class="beatforge-discover-list-item-pills flex flex-col gap-1 bg-primary-850 p-1 rounded-md overflow-x-auto min-w-[130px] whitespace-nowrap">
-            <!-- <div class="flex flex-row gap-1 items-center text-xs text-primary-200">
+	<div
+		class="beatforge-discover-list-item bg-primary-800 hover:bg-primary-850 border-primary-850 my-2 flex w-full min-w-0 flex-row items-center gap-2 rounded-lg border-b-[1px] p-2 transition duration-[120ms] hover:duration-[0ms] md:my-0 md:gap-4 md:rounded-none md:p-4"
+	>
+		<div
+			class="flex flex-shrink-0 w-12 h-12 mr-2 overflow-hidden rounded-md beatforge-discover-list-item-image bg-primary-600 md:h-16 md:w-16"
+		>
+			<img
+				alt={`Listing for mod ${name}, created by ${author}`}
+				src={icon ? icon : '/images/unknown.svg'}
+				class="object-cover w-full h-full"
+			/>
+		</div>
+		<div
+			class="flex flex-col w-full min-w-0 gap-1 truncate beatforge-discover-list-item-info md:gap-1"
+		>
+			<div class="flex items-center gap-2 beatforge-discover-list-item-info-title">
+				<h2 class="font-black text-md md:text-xl">{name}</h2>
+				<p class="text-xs text-primary-200">by {author}</p>
+			</div>
+			<div class="flex min-w-0 beatforge-discover-list-item-description">
+				<p class="min-w-0 text-xs font-medium truncate text-primary-200">
+					{description}
+				</p>
+			</div>
+		</div>
+		<div
+			class="beatforge-discover-list-item-pills bg-primary-850 flex min-w-[130px] flex-col gap-1 overflow-x-auto whitespace-nowrap rounded-md p-1"
+		>
+			<!-- <div class="flex flex-row items-center gap-1 text-xs text-primary-200">
                 <CategoryIcon customClasses="w-4 h-4" />
                 {category} 
             </div> -->
-            <div class="flex flex-row gap-1 items-center text-xs text-primary-200">
-                <DownloadIcon customClasses="flex-shrink-0 w-4 h-4" />
-                {downloads} Downloads
-            </div>
-            <div class="flex flex-row gap-1 items-center text-xs text-primary-200">
-                <CalendarIcon customClasses="flex-shrink-0 w-4 h-4" />
-                {updatedAtProper}
-            </div>
-        </div>
-    </div>
+			<div class="flex flex-row items-center gap-1 text-xs text-primary-200">
+				<DownloadIcon customClasses="flex-shrink-0 w-4 h-4" />
+				{downloads} Downloads
+			</div>
+			<div class="flex flex-row items-center gap-1 text-xs text-primary-200">
+				{category}
+			</div>
+		</div>
+	</div>
 </a>
--- apps/gui/src/routes/discover/+page.svelte
+++ apps/gui/src/routes/discover/+page.svelte
@@ -1,84 +1,108 @@
 <script lang="ts">
-	// import Search from '$lib/components/icons/search.svelte';
-	import { SearchIcon, ChevronDownIcon, ArrowSmallLeftIcon, ArrowSmallRightIcon, FilterIcon } from 'ui/icons';
-	import { Button } from 'ui/button';
-
+	import { SearchIcon, ChevronDownIcon, FilterIcon } from 'ui/icons';
 	import ModListItem from '$lib/components/ModListItem.svelte';
 	import { onMount } from 'svelte';
 
 	enum Sort {
-		Newest = "created_at:desc",
-		Oldest = "created_at:asc",
-		MostPopular = "stats.downloads:desc",
-		LeastPopular = "stats.downloads:asc"
+		Newest = 'created_at:desc',
+		Oldest = 'created_at:asc',
+		MostPopular = 'stats.downloads:desc',
+		LeastPopular = 'stats.downloads:asc'
 	}
 
 	function getSortLabel(sortValue: string): string {
 		switch (sortValue) {
 			case Sort.Newest:
-				return "Newest";
+				return 'Newest';
 			case Sort.Oldest:
-				return "Oldest";
+				return 'Oldest';
 			case Sort.MostPopular:
-				return "Most Popular";
+				return 'Most Popular';
 			case Sort.LeastPopular:
-				return "Least Popular";
+				return 'Least Popular';
 			default:
 				return sortValue;
 		}
 	}
 
-	let listHeight;
+	let scroll_end: HTMLElement;
 	let data: any[] = [];
-	let sort = Sort.Newest;
-	let totalPages = 1;
-	let totalHits = 0;
-	let currentPage = 1;
-	let searchValue: string = ""
+	let existingData: any[] = [];
+
+	$: sort = Sort.Newest;
+	$: searchValue = '';
+	$: currentPage = 1;
+	$: totalPages = 1;
+	$: totalHits = 0;
+	$: gameVersion = '1.28.0';
 
 	const search = async (q: String) => {
 		const res = await (
-			await fetch(`https://search.beatforge.net/indexes/${import.meta.env.MEILI_PREFIX}_mods/search`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					'Access-Control-Allow-Origin': '*',
-					Authorization:
-						'Bearer ' + '1bb9363429a821bb444c459fe529505de0c9985a6abb6802c1849797b4ae316b'
-				},
-				body: JSON.stringify({
-					q: q,
-					sort: [sort.toString()],
-					page: currentPage,
-					hitsPerPage: 5,
-				})
-			})
+			await fetch(
+				`https://search.beatforge.net/indexes/${import.meta.env.MEILI_PREFIX}_mods/search`,
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						'Access-Control-Allow-Origin': '*',
+						Authorization:
+							'Bearer ' + '1bb9363429a821bb444c459fe529505de0c9985a6abb6802c1849797b4ae316b'
+					},
+					body: JSON.stringify({
+						q: q,
+						sort: [sort.toString()],
+						page: currentPage,
+						filter: [[`supported_versions = ${gameVersion}`]]
+					})
+				}
+			)
 		).json();
+
 		data = res.hits;
 		data = data;
 		totalHits = res.totalHits;
 		totalPages = res.totalPages;
+
+		if (currentPage === 1) {
+			existingData = data;
+		} else {
+			existingData = existingData.concat(data);
+		}
 	};
 
-	const searchOnInput = async (e: any) => {
-		await search(<String>(<HTMLInputElement>e.target).value);
-		searchValue = e.target.value;
+	const searchMore = async () => {
+		await search(searchValue);
 		currentPage = 1;
-
 	};
-	const onSortChange = async () => {
-		await search('');
-		currentPage = 1;
-	}
 
 	onMount(async () => {
-		await search('');
+		await searchMore();
+
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting) {
+					if (currentPage < totalPages) {
+						currentPage++;
+						search(searchValue);
+					}
+				}
+			},
+			{
+				root: null,
+				rootMargin: '0px',
+				threshold: 1.0
+			}
+		);
+
+		observer.observe(scroll_end);
 	});
 </script>
 
 <div class="w-full px-6 mx-auto max-w-7xl">
 	<div class="beatforge-discover-header-wrapper">
-		<h1 class="relative max-w-[700px] text-4xl font-black leading-[1.25] tracking-wide mt-4 z-[2]">Discover</h1>
+		<h1 class="relative z-[2] mt-4 max-w-[700px] text-4xl font-black leading-[1.25] tracking-wide">
+			Discover
+		</h1>
 	</div>
 
 	<div class="flex flex-row gap-4 mt-4 beatforge-discover-filter-wrapper">
@@ -87,207 +111,89 @@
 		>
 			<SearchIcon customClasses="w-4 h-4 absolute left-4 z-10 select-none pointer-events-none" />
 			<input
-				on:input={searchOnInput}
+				on:input={searchMore}
+				bind:value={searchValue}
 				class="flex flex-row items-center w-full gap-2 py-3 pl-12"
 				placeholder="Search for mods"
 			/>
 		</div>
 
 		<div
-			class="relative flex flex-row items-center overflow-hidden rounded-md w-14 md:w-2/5 bg-primary-800"
+			class="relative flex flex-row items-center overflow-hidden rounded-md bg-primary-800 w-14 md:w-2/5"
 		>
-			<div class="absolute hidden -translate-y-1/2 pointer-events-none select-none right-4 top-1/2 md:block">
-				<ChevronDownIcon
-					customClasses="w-4 h-4 z-10 select-none pointer-events-none"
-				/>
+			<div
+				class="absolute hidden -translate-y-1/2 pointer-events-none select-none right-4 top-1/2 md:block"
+			>
+				<ChevronDownIcon customClasses="w-4 h-4 z-10 select-none pointer-events-none" />
 			</div>
-			<div class="absolute block -translate-y-1/2 pointer-events-none select-none right-4 top-1/2 md:hidden">
-				<FilterIcon
-					customClasses="w-4 h-4 z-10 select-none pointer-events-none"
-				/>
+			<div
+				class="absolute block -translate-y-1/2 pointer-events-none select-none right-4 top-1/2 md:hidden"
+			>
+				<FilterIcon customClasses="w-4 h-4 z-10 select-none pointer-events-none" />
 			</div>
-			<select class="flex flex-row items-center w-full gap-2 py-3 pl-4 pr-12" bind:value={sort} on:change={onSortChange}>
+			<select
+				class="flex flex-row items-center w-full gap-2 py-3 pl-4 pr-12"
+				bind:value={sort}
+				on:change={searchMore}
+			>
 				<option value={Sort.Newest}>Newest</option>
 				<option value={Sort.Oldest}>Oldest</option>
 				<option value={Sort.MostPopular}>Most popular</option>
 				<option value={Sort.LeastPopular}>Least popular</option>
 			</select>
 		</div>
+
+		<div
+			class="relative flex flex-row items-center overflow-hidden rounded-md bg-primary-800 w-14 md:w-1/5"
+		>
+			<div
+				class="absolute hidden -translate-y-1/2 pointer-events-none select-none right-4 top-1/2 md:block"
+			>
+				<ChevronDownIcon customClasses="w-4 h-4 z-10 select-none pointer-events-none" />
+			</div>
+			<div
+				class="absolute block -translate-y-1/2 pointer-events-none select-none right-4 top-1/2 md:hidden"
+			>
+				<FilterIcon customClasses="w-4 h-4 z-10 select-none pointer-events-none" />
+			</div>
+			<select
+				class="px-4 py-1 text-xs font-bold rounded-md bg-primary-800"
+				bind:value={gameVersion}
+				on:change={searchMore}
+			>
+				{#each ['1.28.0', '1.27.0'] as version}
+					<option value={version}>Version: {version}</option>
+				{/each}
+			</select>
+		</div>
 	</div>
 
-	<div class="flex justify-center p-2 mt-4 -mb-4 rounded-t-md pagination bg-primary-800">
-		<div class="flex overflow-hidden rounded-full">
-			<Button
-			  variant="unset"
-			  on:click={() => {
-				currentPage--;
-				search(searchValue);
-			  }}
-			  disabled={currentPage === 1}
-			  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-			>
-			  <ArrowSmallLeftIcon customClasses="w-4 h-4" />
-			</Button>
-		  
-			{#if totalPages <= 5}
-			  {#each Array(totalPages) as _, i}
-				<Button
-				  variant="unset"
-				  on:click={() => {
-					currentPage = i + 1;
-					search(searchValue);
-				  }}
-				  disabled={currentPage === i + 1}
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  {i + 1}
-				</Button>
-			  {/each}
-			{:else}
-			  {#if currentPage <= 2}
-				{#each Array(4) as _, i}
-				  <Button
-					variant="unset"
-					on:click={() => {
-					  currentPage = i + 1;
-					  search(searchValue);
-					}}
-					disabled={currentPage === i + 1}
-					customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				  >
-					{i + 1}
-				  </Button>
-				{/each}
-				<Button
-				  variant="unset"
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  ...
-				</Button>
-				<Button
-				  variant="unset"
-				  on:click={() => {
-					currentPage = totalPages;
-					search(searchValue);
-				  }}
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  {totalPages}
-				</Button>
-			  {:else if currentPage >= totalPages - 2}
-				<Button
-				  variant="unset"
-				  on:click={() => {
-					currentPage = 1;
-					search(searchValue);
-				  }}
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  1
-				</Button>
-				<Button
-				  variant="unset"
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  ...
-				</Button>
-				{#each Array(4) as _, i}
-				  <Button
-					variant="unset"
-					on:click={() => {
-					  currentPage = totalPages - 3 + i;
-					  search(searchValue);
-					}}
-					disabled={currentPage === totalPages - 3 + i}
-					customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				  >
-					{totalPages - 3 + i}
-				  </Button>
-				{/each}
-			  {:else}
-				<Button
-				  variant="unset"
-				  on:click={() => {
-					currentPage = 1;
-					search(searchValue);
-				  }}
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  1
-				</Button>
-				<Button
-				  variant="unset"
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  ...
-				</Button>
-				{#each Array(2) as _, i}
-				  <Button
-					variant="unset"
-					on:click={() => {
-					  currentPage = currentPage - 1 + i;
-					  search(searchValue);
-					}}
-					disabled={currentPage === currentPage - 1 + i}
-					customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				  >
-					{currentPage - 1 + i}
-				  </Button>
-				{/each}
-				<Button
-				  variant="unset"
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  ...
-				</Button>
-				<Button
-				  variant="unset"
-				  on:click={() => {
-					currentPage = totalPages;
-					search(searchValue);
-				  }}
-				  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-				>
-				  {totalPages}
-				</Button>
-			  {/if}
-			{/if}
-		  
-			<Button
-			  variant="unset"
-			  on:click={() => {
-				currentPage++;
-				search(searchValue);
-			  }}
-			  disabled={currentPage === totalPages}
-			  customClasses="px-3 py-1 rounded-none bg-primary-850 hover:bg-primary-900 text-md font-bold"
-			>
-			  <ArrowSmallRightIcon customClasses="w-4 h-4" />
-			</Button>
-		  </div>
+	<div class="flex mt-4 text-xs font-bold md:hidden">
+		Sorting by<span class="ml-1 text-[#875CEF]">{getSortLabel(sort)}</span>
 	</div>
-	<!-- end pagination -->
-
-	<div class="flex mt-4 text-xs font-bold md:hidden">Sorting by<span class="text-[#875CEF] ml-1">{getSortLabel(sort)}</span></div>
-	<div class="mt-4 overflow-hidden rounded-b-md beatforge-discover-list-items-wrapper">
-		{#if data && data.length}
-			{#each data as mod}
-				<ModListItem
-					name={mod.name}
-					slug={mod.slug}
-					author={mod.author.username}
-					description={mod.description}
-					category={mod.category}
-					downloads={mod.stats.downloads}
-					updatedAt={mod.updated_at}
-					icon={mod.icon}
-				/>
+	<div class="relative mt-4 overflow-hidden beatforge-discover-list-items-wrapper rounded-b-md">
+		{#if existingData.length > 0}
+			{#each existingData as mod}
+				{#key existingData.length}
+					<ModListItem
+						name={mod.name}
+						slug={mod.slug}
+						author={mod.author.username}
+						description={mod.description}
+						category={mod.category}
+						downloads={mod.stats.downloads}
+						icon={mod.icon}
+					/>
+				{/key}
 			{/each}
 		{:else}
 			<p class="text-center text-primary-300">No mods found</p>
 		{/if}
-	</div>
-	<div class="mt-4 text-xs font-black text-primary-300">
-		<p>Total Mods: {totalHits}</p>
+
+		<div class="mt-4 text-xs font-black text-primary-300">
+			<p>Total Mods: {totalHits}</p>
+		</div>
+
+		<div bind:this={scroll_end} class="absolute bottom-[900px] h-1 w-1"></div>
 	</div>
 </div>
