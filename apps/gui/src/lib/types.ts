506d9649c1b5ba9ff0b105be47d3289ffb60a1bf
sargon64 <niko@velvet.moe>
2023-09-10 12:28:28 -0700

    fix: add types

--- apps/gui/src/lib/types.ts
+++ apps/gui/src/lib/types.ts
@@ -1,3 +1,5 @@
+import type { S } from "@tauri-apps/api/event-41a9edf5";
+
 export interface ExistingInstance {
 	id: string;
 	name: string;
@@ -13,4 +15,49 @@ export type Instance = ExistingInstance | null;
 export interface Mod {
 	mod_id: string;
 	timestamp: string;
+}
+
+export interface SearchMod {
+	name: string;
+	description: string;
+	id: string;
+	slug: string;
+	versions: SearchVersion[];
+	category: string;
+	author: SearchAuthor;
+	stats: SearchStats;
+	supported_versions: string[];
+	created_at: string;
+	updated_at: string;
+	status: ModStatus | undefined;
+}
+
+export interface SearchVersion {
+	version: string;
+}
+
+export interface SearchAuthor {
+	username: string;
+	display_name: string | null;
+}
+
+export interface SearchStats {
+	downloads: number;
+}
+
+export enum ModStatus {
+	Installed,
+	NotInstalled,
+	Installing
+}
+
+export interface InstanceUpdate<T extends InstanceUpdateKind | undefined = undefined> {
+	instance_id: number;
+	kind: T;
+}
+
+export interface InstanceUpdateKind {}
+
+export interface ModInstalled extends InstanceUpdateKind {
+	ModInstalled: string;
 }
