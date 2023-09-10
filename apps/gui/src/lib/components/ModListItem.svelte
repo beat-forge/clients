452885a86a9d42661a9e4948cc6dce03254fa0ec
fero <hi@evan.graphics>
2023-09-10 13:04:39 -0700

    style: add spinner to download mod btn

--- apps/gui/src/lib/components/ModListItem.svelte
+++ apps/gui/src/lib/components/ModListItem.svelte
@@ -89,8 +89,10 @@
 					{#if mod.status == ModStatus.Installed}
 						<CheckmarkIcon customClasses="flex-shrink-0 w-4 h-4" />
 					{:else if mod.status == ModStatus.Installing}
-						<!-- todo: add spinner -->
-						<div></div>
+						<svg class="animate-spin w-4 h-4 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+							<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+							<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+						</svg>
 					{:else}
 						<DownloadIcon customClasses="flex-shrink-0 w-4 h-4" />
 					{/if}
