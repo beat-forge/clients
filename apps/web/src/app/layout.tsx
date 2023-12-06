d5ec1e295572e915363067d162dd157d19dd9d6b
PixelBoom <rithikbhatia12@gmail.com>
2023-12-05 19:41:13 -0500

    Prettier

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,23 +1,27 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { GeistSans } from 'geist/font/sans';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import type { Metadata } from "next"
+import "./globals.css"
+import { GeistSans } from "geist/font/sans"
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 
 const apolloClient = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_BEATFORGE_API_URL,
   cache: new InMemoryCache(),
-});
+})
 
 export const metadata: Metadata = {
-  title: 'BeatForge',
-};
+  title: "BeatForge",
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <ApolloProvider client={apolloClient}>
         <body className={GeistSans.className}>{children}</body>
       </ApolloProvider>
     </html>
-  );
+  )
 }
